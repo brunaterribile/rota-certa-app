2c9de57ca482588f1b7d6bde81f4aef5a7ab004c
brunaterribile <brunaterribile@gmail.com>
2024-11-26 00:12:03 -0300

    feat: static home

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
+import { FormContainer, HomeContainer, RequestRideButton } from "./styles";
+
 export function Home() {
     return (
-        <div>
-    <h2> Insira os dados para solicitar uma viagem: </h2>
+        <HomeContainer>
+            <h2> Insira os dados para solicitar uma viagem: </h2>
+            <form action="">
+                <FormContainer>
+                    <label htmlFor="customer_id">Id do usuário</label>
+                    <input id="customer_id"/>
 
-        </div>
+                    <label htmlFor="orgin">Endereço de partida</label>
+                    <input id="orgin" type="text"/>
+
+                    <label htmlFor="destination">Endereço de destino</label>
+                    <input id="destination" type="text"/>
+                </FormContainer>
+                <RequestRideButton type="submit">Solicitar</RequestRideButton>
+            </form>
+        </HomeContainer>
     )
 }
---
+++ front/src/pages/Home/styles.ts
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+    max-width: 74rem;
+    height: calc(100vh -10rem);
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 3.5rem;
+    }
+
+    h2 {
+        margin: 2rem 0;
+    }
+
+    //background-color: cadetblue;
+`;
+
+export const FormContainer = styled.main`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 0.5rem;
+    color: ${props => props.theme['gray-100']};
+    font-size: 1.3rem;
+    font-weight: bold;
+    flex-wrap: wrap;
+
+    input {
+        width: 100%;
+        height: 2.5rem;
+        padding: 0.8rem;
+        border: 2px solid transparent;
+        border-radius: 8px;
+        outline: none;
+        background: ${props => props.theme['gray-100']};
+        color: ${props => props.theme['gray-800']};
+        font-weight: bold;
+        font-size: 1.125rem;
+        box-shadow: none;
+        box-sizing: border-box;
+        
+        &:focus {
+            border: 2px solid ${props => props.theme['green-500']};
+        }
+    }
+`;
+
+export const RequestRideButton = styled.button`
+    width: 40%;
+    padding: 0.7rem 1.6rem;
+    border: none;
+    border-radius: 8px;
+    background: ${props => props.theme['green-500']};
+    color: ${props => props.theme['gray-100']};
+    font-weight: bold;
+    font-size: 1.3rem;
+    cursor: pointer;
+
+    &:disabled {
+        opacity: 0.7;
+        cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
+        background: ${props => props.theme['green-700']};
+    }
+`;
