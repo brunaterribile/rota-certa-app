7987bfdd945f9c9c7c6bf8bd9a7be1255cca2f64
brunaterribile <brunaterribile@gmail.com>
2024-11-26 21:57:01 -0300

    fix: add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,12 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import cors from '@fastify/cors'
 
 export const app = fastify()
+
+app.register(cors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+})
 
 app.register(appRoutes)
