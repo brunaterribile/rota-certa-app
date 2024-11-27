c40f6523f2d9e46e13309dd38169522c47a978aa
brunaterribile <brunaterribile@gmail.com>
2024-11-27 15:00:48 -0300

    feat: config docker compose

--- front/src/components/Map/index.tsx
+++ front/src/components/Map/index.tsx
@@ -5,17 +5,17 @@ type StaticMapProps = {
 };
 
 export const StaticMap = ({ routeResponse }: StaticMapProps) => {
-  const googleMapsAPIKey = "AIzaSyCLVgPY8En6RqlroAjDtk8N_gNSCOamxtw"
+  const googleMapsAPIKey = process.env.GOOGLE_API_KEY
 
   const startLocation = routeResponse.legs[0].startLocation.latLng;
   const endLocation = routeResponse.legs[0].endLocation.latLng;
   const polyline = encodeURIComponent(routeResponse.polyline.encodedPolyline);
-console.log(polyline)
+
   const centerLat = (startLocation.latitude + endLocation.latitude) / 2;
   const centerLng = (startLocation.longitude + endLocation.longitude) / 2;
 
-  const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${centerLat},${centerLng}&zoom=14&size=600x400&markers=color:blue%7Clabel:A%7C${startLocation.latitude},${startLocation.longitude}&markers=color:red%7Clabel:B%7C${endLocation.latitude},${endLocation.longitude}&path=enc:${polyline}&key=${googleMapsAPIKey}`;
-console.log(mapUrl)
+  const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${centerLat},${centerLng}&zoom=12&size=600x400&markers=color:blue%7Clabel:A%7C${startLocation.latitude},${startLocation.longitude}&markers=color:red%7Clabel:B%7C${endLocation.latitude},${endLocation.longitude}&path=enc:${polyline}&key=${googleMapsAPIKey}`;
+
   return (
     <div>
       <img src={mapUrl} alt="Mapa Estático com Rota" />
